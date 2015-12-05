734e91a3ef8cc92ffb8efe4c0af1ea171d9c6198
Aaron McLeod <aaron.g.mcleod@gmail.com>
2015-12-05 18:05:16 -0500

    drawing on screen now.

--- core/src/com/agmcleod/mytestgame/MyTestGame.java
+++ core/src/com/agmcleod/mytestgame/MyTestGame.java
@@ -7,12 +7,10 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MyTestGame extends ApplicationAdapter {
-    SpriteBatch batch;
     private Engine engine;
     Texture img;
     private Stage stage;
@@ -21,7 +19,6 @@ public class MyTestGame extends ApplicationAdapter {
     @Override
     public void create () {
         engine = new Engine();
-        batch = new SpriteBatch();
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         textureManager = new TextureManager();
@@ -44,6 +41,8 @@ public class MyTestGame extends ApplicationAdapter {
         float dt = Gdx.graphics.getDeltaTime();
         Gdx.gl.glClearColor(0.15f, 0.22f, 0.47f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        engine.update(dt);
 
         stage.act(dt);
         stage.draw();
